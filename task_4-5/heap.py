3a40c19da8ab4fdf34be5e00d6c6a673bb0ad66b
Natalia <icxodnik@gmail.com>
2025-05-12 15:33:37 +0200

    refactoring, reuse tree

--- task_4-5/heap.py
+++ task_4-5/heap.py
@@ -2,44 +2,7 @@ import uuid
 
 import networkx as nx
 import matplotlib.pyplot as plt
-
-
-class Node:
-    def __init__(self, key, color="skyblue"):
-        self.left = None
-        self.right = None
-        self.val = key
-        self.color = color # Додатковий аргумент для зберігання кольору вузла
-        self.id = str(uuid.uuid4()) # Унікальний ідентифікатор для кожного вузла
-
-    
-def add_edges(graph, node, pos, x=0, y=0, layer=1):
-    if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val) # Використання id та збереження значення вузла
-        if node.left:
-            graph.add_edge(node.id, node.left.id)
-            l = x - 1 / 2 ** layer
-            pos[node.left.id] = (l, y - 1)
-            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
-        if node.right:
-            graph.add_edge(node.id, node.right.id)
-            r = x + 1 / 2 ** layer
-            pos[node.right.id] = (r, y - 1)
-            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
-    return graph
-
-
-def draw_tree(tree_root):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
-
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)} # Використовуйте значення вузла для міток
-
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.show()
+from tree import Node
 
 class BinaryHeap:
     def __init__(self, max_heap=True):
@@ -68,22 +31,23 @@ class BinaryHeap:
         for i in range(len(self.heap) // 2 - 1, -1, -1):
             self._heapify_down(i)
 
-# Створення купи
-heap = BinaryHeap(max_heap=False)
-heap.build([0, 4, 5, 10, 1, 3])
+if __name__ == "__main__":
+    # Створення купи
+    heap = BinaryHeap(max_heap=False)
+    heap.build([0, 4, 5, 10, 1, 3])
 
-# Відображення купи
-root = Node(heap.heap[0])
-nodes = [root]
-for val in heap.heap[1:]:
-    node = Node(val)
-    nodes.append(node)
+    # Відображення купи
+    root = Node(heap.heap[0])
+    nodes = [root]
+    for val in heap.heap[1:]:
+        node = Node(val)
+        nodes.append(node)
 
-# Створюємо дерево з елементів купи
-for i, node in enumerate(nodes):
-    if 2 * i + 1 < len(nodes):
-        node.left = nodes[2 * i + 1]
-    if 2 * i + 2 < len(nodes):
-        node.right = nodes[2 * i + 2]
+    # Створюємо дерево з елементів купи
+    for i, node in enumerate(nodes):
+        if 2 * i + 1 < len(nodes):
+            node.left = nodes[2 * i + 1]
+        if 2 * i + 2 < len(nodes):
+            node.right = nodes[2 * i + 2]
 
-draw_tree(root)
+    Node.draw_tree(root)
