9b96c4e02b70f6ed1948414684a98c4d5fe8f880
Natalia <icxodnik@gmail.com>
2025-05-12 13:54:53 +0200

    add technical function for debugging

--- task_1/linked_list.py
+++ task_1/linked_list.py
@@ -2,6 +2,12 @@ class Node:
     def __init__(self, data=None):
         self.data = data
         self.next = None
+
+    def __repr__(self):
+        return f"Node({self.data})"
+    
+    def __str__(self):
+        return self.__repr__()
         
 class LinkedList:
     def __init__(self):
