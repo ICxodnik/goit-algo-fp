ae9c672611a75bd3d06403eff15ebdde7c0f371c
Natalia <icxodnik@gmail.com>
2025-05-12 22:25:01 +0200

    add graph

--- task_7/monte-carlo.py
+++ task_7/monte-carlo.py
@@ -11,12 +11,19 @@ for _ in range(num_simulations):
     total = die1 + die2
     results[total] += 1
 
-
-probabilities = {k: v / num_simulations for k, v in results.items()}
-
+probabilities = {k: (v / num_simulations) * 100 for k, v in results.items()}
 
 print("Сума | Ймовірність")
 print("-------------------")
 for sum_, prob in probabilities.items():
-    print(f"{sum_:>4} | {prob:.5f}")
+    print(f"{sum_:>4} | {prob: .2f}")
 
+# Побудова графіка
+plt.figure(figsize=(10, 6))
+plt.bar(probabilities.keys(), probabilities.values(), color='skyblue', edgecolor='black')
+plt.xlabel("Сума на кубиках")
+plt.ylabel("Ймовірність %")
+plt.title(f"Ймовірності сум при {num_simulations:,} кидках двох кубиків")
+plt.xticks(range(2, 13))
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
