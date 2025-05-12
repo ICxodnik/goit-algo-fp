1ef0b7c8299690ef1f903034a9544341054ae91b
Natalia <icxodnik@gmail.com>
2025-05-12 22:51:59 +0200

    add mae calculation

--- task_7/monte-carlo.py
+++ task_7/monte-carlo.py
@@ -1,8 +1,6 @@
 import random
 import matplotlib.pyplot as plt
 
-
-
 def die_emulation(num_simulations):
     results = {i: 0 for i in range(2, 13)}
 
@@ -33,9 +31,48 @@ def build_picture(num_simulations, probabilities):
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
 
+def build_error_graph(simulation_counts, mae_values):
+    plt.figure(figsize=(8, 5))
+    plt.plot(simulation_counts, mae_values, marker='o', linestyle='-', color='green')
+    plt.xlabel("Кількість симуляцій")
+    plt.ylabel("Середня абсолютна похибка %")
+    plt.title("Залежність точності від кількості симуляцій")
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.xscale('log')
+    plt.tight_layout()
+    plt.show()
+
+# Теоретичні ймовірності для кожної суми
+analytical_probs = {
+    2: 1/36 * 100,
+    3: 2/36 * 100,
+    4: 3/36 * 100,
+    5: 4/36 * 100,
+    6: 5/36 * 100,
+    7: 6/36 * 100,
+    8: 5/36 * 100,
+    9: 4/36 * 100,
+    10: 3/36 * 100,
+    11: 2/36 * 100,
+    12: 1/36 * 100
+}
+
+# Функція для обчислення середньої абсолютної похибки
+def calculate_mae(simulated_probs):
+    error = 0
+    for k in analytical_probs:
+        error += abs(simulated_probs[k] - analytical_probs[k])
+    return (error / len(analytical_probs))
+
 if __name__ == "__main__":
-    num_simulations = 100_000
-    for num_simulations in [10_000, 100_000, 1_000_000]:
+    mae_values = []
+    simulation_counts = [1_000, 100_000, 1_000_000]
+
+    for num_simulations in simulation_counts:
         probabilities = die_emulation(num_simulations)
         build_picture(num_simulations, probabilities)
+        mae = calculate_mae(probabilities)
+        mae_values.append(mae)
+        print(f"Похибка для {num_simulations:,} кидків: {mae*100:.2f}")
 
+    build_error_graph(simulation_counts, mae_values)
